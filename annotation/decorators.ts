0550d80f47d1f4ab882f1fbff3fdbcf2726e86f8
Isaac <hizikugwuanya@gmail.com>
2023-04-28 10:09:05 +0100

    my typescript journey

--- annotation/decorators.ts
+++ annotation/decorators.ts
@@ -1,22 +1,37 @@
+@classDecorator
 class Boat {
-    @testDecorator
     color: string = 'red';
 
+    @testDecorator
     get formattedColor(): string {
         return `This boats color is ${this.color}`;
     }
 
-    @logError('Oops, boat was sunk in the ocean')
-    pilot(): void {
-        throw new Error( )
-        console.log('Mitoma');
+    @logError('Something bad')
+    pilot(@parameterDecorator speed: string,
+         @parameterDecorator generateWake: boolean): void {
+        if (speed === 'fast') {
+            console.log('Mitoma')
+        } else {
+            console.log('nothing')
+        }
     }
 }
 
-function testDecorator(target: any, key: string) {
-    console.log(target.color);
-    console.log(key);
+//for parameter
+function parameterDecorator (taarget: any, key: string, index: number) {
+    console.log(key, index);
     
+}
+
+//for class
+function classDecorator (constructor: typeof Boat) {
+    console.log(constructor);
+    
+}
+
+function testDecorator(target: any, key: string) {
+    console.log(key);
  }
 
 function logError(errorMessage: string) {
