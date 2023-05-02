b7b17f75a73fcc054a3d735fa8a2636114dd3579
Isaac <hizikugwuanya@gmail.com>
2023-05-02 09:47:35 +0100

    my typecript journey

--- annotation/metadata.ts
+++ annotation/metadata.ts
@@ -2,26 +2,29 @@
 
 import 'reflect-metadata'
 
-@printMetadata
+@controller
 class Plane {
     color: string = 'red'
 
-    @markFunction('Hi Johann')
+    @get('/login')
     fly(): void {
         console.log('Vrrrrrrr');
     }
 };
 
-function markFunction(secretInfo: string) {
+function get(path: string) {
     return function(target: Plane, key: string) {
-        Reflect.defineMetadata('secret', secretInfo, target, key)
+        Reflect.defineMetadata('path', path, target, key)
     }
 }
 
-function printMetadata(target: typeof Plane) {
+function controller(target: typeof Plane) {
     for (let key in target.prototype) {
-        const secret = Reflect.getMetadata('secret', target.prototype, key) 
-            console.log(secret);
+        const path = Reflect.getMetadata('path', target.prototype, key);
+        const middleware = Reflect.getMetadata('middleware', target.prototype);
+
+        router.get(path, middleware, target.prototype[key])
+
     }
 }
 
